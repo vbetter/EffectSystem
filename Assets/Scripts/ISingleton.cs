eb2288ebc5771ba65e285641d947294c8a9338d3
vbetter <wodemer@me.com>
2018-01-21 23:37:02 +0800

    update

---
+++ Assets/Scripts/ISingleton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ISingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static GameObject m_Container = null;  
+	private static T instance;
+	public static T Instance
+	{
+		get
+		{
+            if (!instance)
+            {
+                instance = GameObject.FindObjectOfType<T>();  
+            }
+            if (!instance)
+            {
+				#if Profile
+				Profiler.BeginSample("Singleton.NewGameObject");
+				#endif
+                GameObject obj = new GameObject(typeof(T).ToString());
+                instance = obj.AddComponent<T>();
+				#if Profile
+				Profiler.EndSample();
+				#endif
+            }
+			return instance;
+		}
+	}
+
+
+
+	public static T Initialize() { return Instance; }
+	public static bool IsCreated() { return (instance != null); }
+}
